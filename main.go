593be8d44fec9ac9d1696f64096b2cf873d8a1c4
axzilla <hello@axeladrian.com>
2025-08-16 12:18:50 +0300

    chore: remove www routing

--- main.go
+++ main.go
@@ -19,22 +19,7 @@ func main() {
 	mux.Handle("GET /", templ.Handler(pages.Landing()))
 	fmt.Println("Server is running on http://localhost:8090")
 
-	// Simple www to non-www redirect
-	redirectWWW := func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if after, ok := strings.CutPrefix(r.Host, "www."); ok {
-				target := "https://" + after + r.URL.Path
-				if r.URL.RawQuery != "" {
-					target += "?" + r.URL.RawQuery
-				}
-				http.Redirect(w, r, target, http.StatusMovedPermanently)
-				return
-			}
-			next.ServeHTTP(w, r)
-		})
-	}
-
-	http.ListenAndServe(":8090", redirectWWW(mux))
+	http.ListenAndServe(":8090", mux)
 }
 
 func InitDotEnv() {
